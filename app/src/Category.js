945beb419b31fe71a08f4906323e18820780d064
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-02-17 18:46:00 +0100

    Building a front end using React

--- app/src/Category.js
+++ app/src/Category.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
+import AppNav from './AppNav';
 
 class Category extends Component {
-    state = { 
-      isLoading : true,
-      Categories : []
-     }
 
-     //async
-     async componentDidMount(){
-         const response = await fetch('api/categories');
-         const body = await response.json();
-         this.setState({Categories : body, isLoading : false});
-     }
+    state = {  
+        isLoading : true,
+        Categories : []
+    }
+ 
+    async componentDidMount(){
+        const response=await fetch('/api/categories');
+        const body = await response.json();
+        this.setState({Categories : body , isLoading: false});
+    }
 
-
-    render() {
-        const {Categories, isLoading} = this.state;
-        if(isLoading)
-            return(<div>Loading...</div>)
+    render() { 
+        const {Categories , isLoading} = this.state;
+        if(isLoading) 
+            return (<div>Loading...</div>);
+        
         return ( 
-            <div>
-                <h2>Categories</h2>
-                {
-                    Categories.map(category =>
-                        <div id = {category.name}>
-                            {category.name}
-                        </div>
+            
+                <div>
+                    <AppNav/>
+                    <h2>Categories</h2>
+                    {
+                        Categories.map( category => 
+                            <div id={category.id}>
+                                {category.name}
+                            </div>
                         )
-                 }    
 
-            </div>
+                    }
+
+                </div>
          );
     }
 }
